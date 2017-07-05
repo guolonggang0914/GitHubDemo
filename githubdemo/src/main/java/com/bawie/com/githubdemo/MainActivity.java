d6eb27740dc8ae7493642e16f9d0779cfe584d8b
guolonggang0914 <2363232934@qq.com>
2017-07-05 20:06:05 +0800

    这是我自己修改之后的类

--- githubdemo/src/main/java/com/bawie/com/githubdemo/MainActivity.java
+++ githubdemo/src/main/java/com/bawie/com/githubdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.bawie.com.githubdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("郭龙刚。。。");
+        Log.i("MainActivity","ssssss");
     }
 }
