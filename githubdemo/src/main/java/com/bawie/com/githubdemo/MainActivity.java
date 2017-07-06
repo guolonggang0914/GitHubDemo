7116896d595f16bf91f703e390e2c97340869019
MessZW <304609860@qq.com>
2017-07-06 11:00:13 +0800

    张伟真帅

--- githubdemo/src/main/java/com/bawie/com/githubdemo/MainActivity.java
+++ githubdemo/src/main/java/com/bawie/com/githubdemo/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("郭龙刚。。。");
         Log.i("MainActivity","ssssss");
+        //sdfghjkl;wsdfghjkl;
     }
 }
