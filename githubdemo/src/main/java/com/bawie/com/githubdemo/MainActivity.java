b03af913f959981870a025a00ce36cd2b64be742
Jack1378 <765155530@qq.com>
2017-07-06 11:06:18 +0800

    rgrtgbbfds

--- githubdemo/src/main/java/com/bawie/com/githubdemo/MainActivity.java
+++ githubdemo/src/main/java/com/bawie/com/githubdemo/MainActivity.java
@@ -12,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("郭龙刚。。。");
         Log.i("MainActivity","ssssss");
+
+
+//        asdfn bfwe
     }
 }
